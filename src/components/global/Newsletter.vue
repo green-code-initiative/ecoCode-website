cc44047941dd02a537d47be0ca1c502233db8622
bradley <bdesubercaseaux@snapp.fr>
2023-10-18 13:52:21 +0200

    spelling correction for error send form

--- src/components/global/Newsletter.vue
+++ src/components/global/Newsletter.vue
@@ -63,7 +63,7 @@ const submitForm = async () => {
       const response = await axios.post("https://api.ecocode.io/newsletter", formData, headers);
       success.value = "Votre demande a bien été enregistrée";
     } catch (err) {
-      error.value = "Erreur d'envoie, veuillez réessayer plus tard.";
+      error.value = "Erreur d'envoi, veuillez réessayer plus tard.";
     }
   }
 };
