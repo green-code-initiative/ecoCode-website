d180d4eab36b8747519659a0337197c5fad7094c
utarwyn <maxime.malgorn@laposte.net>
2024-01-06 16:13:41 +0100

    Fix types

--- src/global.d.ts
+++ src/global.d.ts
@@ -2,7 +2,7 @@ declare type MemberLinkType = 'linkedin' | 'github';
 
 declare type Member = {
     name: string
-    company: string
+    company?: string
     profile: string
     links: Partial<Record<MemberLinkType, string>>
 }
